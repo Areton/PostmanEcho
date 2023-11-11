28995cf5bfa2f19111ca1ddf3b57218823095246
Никитосик <nik-ar98@mail.ru>
2023-11-11 17:55:05 +0500

    Different data

---
+++ src/test/java/ru/netology/PostmanEchoTest.java
@@ -0,0 +1,19 @@
+package ru.netology;
+
+import org.junit.jupiter.api.Test;
+import static org.hamcrest.Matchers.equalTo;
+import static io.restassured.RestAssured.given;
+public class PostmanEchoTest {
+
+    @Test
+    void shouldReturnSendData() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("hi")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("Hi"));
+    }
+}
